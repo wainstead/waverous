adde8aa365c06587d428afff4aeb03be27e59956
nop <nop>
1998-12-29 06:56:32 +0000

    Fixed leak in onc().

--- server/server.c
+++ server/server.c
@@ -1365,8 +1365,10 @@ bf_open_network_connection(Var arglist, Byte next, void *vdata, Objid progr)
     Var r;
     enum error e;
 
-    if (!is_wizard(progr))
-	return make_error_pack(E_PERM);
+    if (!is_wizard(progr)) {
+        free_var(arglist);
+        return make_error_pack(E_PERM);
+    }
 
     e = network_open_connection(arglist);
     free_var(arglist);
@@ -1733,10 +1735,13 @@ register_server(void)
 		      bf_buffered_output_length, TYPE_OBJ);
 }
 
-char rcsid_server[] = "$Id: server.c,v 1.4 1998-12-14 13:18:57 nop Exp $";
+char rcsid_server[] = "$Id: server.c,v 1.5 1998-12-29 06:56:32 nop Exp $";
 
 /* 
  * $Log: not supported by cvs2svn $
+ * Revision 1.4  1998/12/14 13:18:57  nop
+ * Merge UNSAFE_OPTS (ref fixups); fix Log tag placement to fit CVS whims
+ *
  * Revision 1.3  1997/03/03 05:03:52  nop
  * steak2: move protectedness into builtin struct, load_server_options()
  * now required for $server_options updates.
