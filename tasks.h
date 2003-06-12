f72e12a4f3e2db57d8f572d1bf31c579018ee25e
Ben Jackson <ben@ben.com>
2003-06-12 18:16:57 +0000

    Suspend input on connection until :do_login_command() can run.

--- tasks.h
+++ tasks.h
@@ -36,6 +36,7 @@ extern int tasks_set_connection_option(task_queue, const char *,
 				       Var);
 
 extern void new_input_task(task_queue, const char *);
+extern void task_suspend_input(task_queue);
 extern enum error enqueue_forked_task2(activation a, int f_index,
 			       unsigned after_seconds, int vid);
 extern enum error enqueue_suspended_task(vm the_vm, void *data);
@@ -110,6 +111,9 @@ extern db_verb_handle find_verb_for_programming(Objid player,
 
 /* 
  * $Log: not supported by cvs2svn $
+ * Revision 1.3  1998/12/14 13:19:08  nop
+ * Merge UNSAFE_OPTS (ref fixups); fix Log tag placement to fit CVS whims
+ *
  * Revision 1.2.2.1  1998/12/06 07:13:23  bjj
  * Rationalize enqueue_forked_task interface and fix program_ref leak in
  * the case where fork fails with E_QUOTA.  Make .queued_task_limit=0 really
