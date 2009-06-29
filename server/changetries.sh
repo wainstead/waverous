4a46800565f83ca3df7fa943831bf561a351f12c
Steve Wainstead <wainstead@gmail.com>
2009-06-29 19:38:29 +0000

    cleaning up scripts used to massage the code base

--- server/changetries.sh
+++
@@ -1,6 +0,0 @@
-# decompile.c has e->e._catch.try = try_expr;
-perl -pi -e 's/\Qe->e._catch.try = try_expr;\E/e->e._catch._try = _try_expr;/' decompile.c
-# unparse.c has unparse_expr(str, expr->e._catch.try);
-perl -pi -e 's/\Qunparse_expr(str, expr->e._catch.try);\E/unparse_expr(str, expr->e._catch._try);/' unparse.c
-# parser.y has $$->e._catch.try = $2;
-perl -pi -e 's/\Q$$->e._catch.try = $2;\E/$$->e._catch._try = $2;/' parser.y
