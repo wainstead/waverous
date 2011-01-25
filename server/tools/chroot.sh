0fe582b4d3cae4f9ce785d10e5d4f790a76d62aa
Steve Wainstead <wainstead@gmail.com>
2011-01-25 17:25:21 +0000

    Creating a chrooted environment to run waverous in...

---
+++ server/tools/chroot.sh
@@ -0,0 +1,41 @@
+# on a Fedora 14 system, here's how I created a chrooted environment
+# to run the server in...
+
+# path to your project, which should be compiled
+MOOBIN=~swain/public_html/projects/waverous/server
+
+# path to the chroot directory you will use
+CHROOTDIR=/chroot/waverous
+
+mkdir -p $CHROOTDIR
+cd $CHROOTDIR
+mkdir -p lib usr/lib
+mkdir bin sbin logs databases
+
+cp $MOOBIN/moo $CHROOTDIR/sbin/
+cp $MOOBIN/databases/* $CHROOTDIR/databases/
+
+# for the moo binary: run 'ldd moo' to see the list of libraries it
+# needs, then copy them to $CHROOTDIR...
+cp /lib/libexpat.so.1  lib/
+cp /lib/libcrypt.so.1  lib/
+cp /lib/libm.so.6      lib/
+cp /lib/libgcc_s.so.1  lib/
+cp /lib/libc.so.6      lib/
+cp /lib/libfreebl3.so  lib/
+cp /lib/ld-linux.so.2  lib/
+cp /lib/libdl.so.2     lib/
+
+cp /usr/lib/libstdc++.so.6 usr/lib
+
+# for bash, if you want to run restart.sh...
+cp /lib/libtinfo.so.5 lib/
+cp /bin/bash bin/
+cd bin
+chmod a+x bash
+ln -s bash sh
+cd ..
+
+# running the server; but running it via restart.sh would be better,
+# because your changes are lost otherwise
+#chroot $CHROOTDIR /sbin/moo -l /logs/my.log /databases/LambdaCore-12Apr99.db /databases/LambdaCore-12Apr99.mine.db
